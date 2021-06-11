dc427c6ab4a9efbf5f55521c41e1f3595fea977c
Jay Butera <buterajay@protonmail.com>
2021-06-11 13:35:59 -0500

    Progress on flake

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.naersk.url = "github:nmattia/naersk";
-  #inputs.melwalletd.url = "https://github.com/themeliolabs/melwalletd";
+  #inputs.naersk.url = "github:nmattia/naersk";
+  inputs.melwalletd-flake.url = "github:themeliolabs/melwalletd";
   inputs.mozilla = { url = "github:mozilla/nixpkgs-mozilla"; flake = false; };
   inputs.tauriSrc = { url = "github:tauri-apps/tauri/cli.js-v1.0.0-beta.2"; flake = false; };
 
@@ -14,20 +14,19 @@
     , mozilla
     , flake-utils
     , tauriSrc
-    , naersk
-    #, melwalletd
+    , melwalletd-flake
     , ...
     } @inputs:
     let rustOverlay = final: prev:
-          let rustChannel = prev.rustChannelOf {
-            channel = "1.52.0";
-            sha256 = "sha256-fcaq7+4shIvAy0qMuC3nnYGd0ZikkR5ln/rAruHA6mM=";
-          };
-          in
-          { inherit rustChannel;
-            rustc = rustChannel.rust;
-            cargo = rustChannel.rust;
-          };
+      let rustChannel = prev.rustChannelOf {
+        channel = "1.52.0";
+        sha256 = "sha256-fcaq7+4shIvAy0qMuC3nnYGd0ZikkR5ln/rAruHA6mM=";
+      };
+      in
+      { inherit rustChannel;
+        rustc = rustChannel.rust;
+        cargo = rustChannel.rust;
+      };
 
     in flake-utils.lib.eachSystem
       ["x86_64-linux"]
@@ -41,7 +40,27 @@
           ];
         };
 
-        naersk-lib = naersk.lib."${system}";
+        rustPlatform = let rustChannel = pkgs.rustChannelOf {
+            channel = "1.52.0";
+            sha256 = "sha256-fcaq7+4shIvAy0qMuC3nnYGd0ZikkR5ln/rAruHA6mM=";
+          }; in
+            pkgs.makeRustPlatform {
+              cargo = rustChannel.rust;
+              rustc = rustChannel.rust;
+            };
+
+        #naersk-lib = naersk.lib."${system}";
+        tauri = rustPlatform.buildRustPackage rec {
+          pname = "tauri-v${version}";
+          version = "1.0.0-beta.2";
+          src = "${tauriSrc}";
+          sourceRoot = "source/tooling/cli.rs";
+          cargoSha256 = "sha256-v1dFLI8J3Ksg+lkw9fAwTYytXkj3ZLlB6086LPy9ZxY=";
+        };
+
+        melwalletd = melwalletd-flake.packages."${system}".melwalletd;
+
+        bundleDrv = pkgs.callPackage ./bundle.nix {};
 
         tauri-deps = with pkgs; [
               binutils
@@ -73,6 +92,12 @@
             ] ++ tauri-deps;
           };
           */
+          melwalletd = melwalletd-flake.packages."${system}".melwalletd;
+
+          packages.tauri = tauri;
+
+          # TODO this should not be
+          defaultPackage = packages.tauri;
 
           devShell = pkgs.mkShell {
             buildInputs = with pkgs; [
@@ -84,8 +109,15 @@
               export OPENSSL_DIR="${pkgs.openssl.dev}"
               export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
 
+              # melwalletd
+              export PATH=$PATH:${melwalletd}/bin
+              # Place into project for bundling
+              cp ${melwalletd}/bin/melwalletd ${self}/src-tauri/
+
+              # Tauri cli
+              export PATH=$PATH:${packages.tauri}/bin
+              alias tauri='cargo-tauri'
             '';
-              #cp ${melwalletd}/bin/melwalletd ./bin
           };
         });
 }
