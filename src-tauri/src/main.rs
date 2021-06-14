62fa6a7a9c7b6a5389dd4706bc38c4461778a1e5
Jay Butera <buterajay@protonmail.com>
2021-06-14 16:47:18 -0500

    Kill melwaleltd on successful wallet close

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,12 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::api::process::Command;
+use std::path::Path;
+use tauri::api::process::{Command, CommandChild, CommandEvent};
+use tauri::async_runtime::Receiver;
 
-use std::{
-    path::Path,
-};
-
-//fn start_walletd(wallet_dir: &Path) -> Result<(Receiver<CommandEvent>, CommandChild)> {
-fn start_walletd(wallet_dir: &Path) {
+fn start_walletd(wallet_dir: &Path) -> tauri::Result<(Receiver<CommandEvent>, CommandChild)> {
+//fn start_walletd(wallet_dir: &Path) {
     let wallet_path_str = wallet_dir.to_str()
         .expect(&format!("Invalid wallet directory path, {}", wallet_dir.to_string_lossy()));
 
@@ -23,15 +21,20 @@ fn start_walletd(wallet_dir: &Path) {
         .args(&["--testnet-connect", "94.237.109.44:11814"])
         .args(&["--wallet-dir", wallet_path_str])
         .spawn()
-        .expect("melwalletd failed to start");
+        .map_err(|e| tauri::Error::FailedToExecuteApi(e))
 }
 
 fn main() {
     // Spawn wallet daemon as a child process
     let wallet_dir = Path::new("./wlt");
-    start_walletd(wallet_dir);
+    let (_, cmd) = start_walletd(wallet_dir)
+        .expect("melwalletd failed to start");
 
     tauri::Builder::default()
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    // Kill melwalletd
+    cmd.kill()
+        .expect("Failed to kill melwalletd");
 }
